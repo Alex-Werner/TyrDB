6622d2f34ba64b17a93ebedff3526bb5fa689c48
Alex Werner <alex@werner.fr>
2020-08-29 06:27:28 +0200

    chore: modified scripts

--- src/types/Collection/Collection.spec.js
+++ src/types/Collection/Collection.spec.js
@@ -1,23 +1,23 @@
 const { expect } = require('chai');
 
-const Collection = require('../../../src/types/Collection/Collection');
+const CollectionSpec = require('./Collection');
 const {MemoryAdapter} = require('../../../adapters')
 describe('Type - Collection', () => {
   it('should work', function () {
     const fakeTyrInstance = {
       persistanceAdapter: new MemoryAdapter()
     };
-    const col = new Collection({name:'users', tyrInstance:fakeTyrInstance})
+    const col = new CollectionSpec({name:'users', tyrInstance:fakeTyrInstance})
     expect(col.name).to.be.deep.equal("users")
-    const colUnique = new Collection({name:'users', tyrInstance:fakeTyrInstance,uniques:['email']})
+    const colUnique = new CollectionSpec({name:'users', tyrInstance:fakeTyrInstance,uniques:['email']})
     expect(colUnique.name).to.be.deep.equal("users")
     expect(colUnique.getTree().uniques).to.be.deep.equal(['email'])
 
-    const colExclude = new Collection({name:'users', tyrInstance:fakeTyrInstance,exclude:['email']})
+    const colExclude = new CollectionSpec({name:'users', tyrInstance:fakeTyrInstance,exclude:['email']})
     expect(colExclude.name).to.be.deep.equal("users")
     expect(colExclude.getTree().exclude).to.be.deep.equal(['email'])
 
-    const colOrder = new Collection({name:'users', tyrInstance:fakeTyrInstance,order:25})
+    const colOrder = new CollectionSpec({name:'users', tyrInstance:fakeTyrInstance,order:25})
     expect(colOrder.getTree().order).to.be.deep.equal(25)
   });
 });
--- src/types/Event.spec.js
+++ src/types/Event.spec.js
@@ -1,12 +1,12 @@
 const { expect } = require('chai');
-const Event = require('../../src/types/Event');
+const EventSpec = require('./Event');
 describe('Type - Event', () => {
   it('should work', function () {
-    const event = new Event('started');
+    const event = new EventSpec('started');
     expect(event.name).to.equal('started');
     expect(event.payload).to.deep.equal({});
 
-    const event2 = new Event({name:'started', payload:{isStarted:true}})
+    const event2 = new EventSpec({name:'started', payload:{isStarted:true}})
     expect(event2.name).to.equal('started');
     expect(event2.payload).to.deep.equal({isStarted:true});
   });
