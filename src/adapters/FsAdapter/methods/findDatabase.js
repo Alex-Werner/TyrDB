d19129dbf5d30af4f46b3d0c36cd13806a5a8555
Alexey Vedmedenko <vedmalex@gmail.com>
2020-12-31 00:11:58 +0300

    fix issue #1

--- src/adapters/FsAdapter/methods/findDatabase.js
+++ src/adapters/FsAdapter/methods/findDatabase.js
@@ -1,21 +1,24 @@
 const Database = require('../../../types/Database/Database');
-module.exports = async function findDatabase(tyrInstance, dbName, opts){
-  let db = undefined;
-  if(!dbName){
-    throw new Error("Expected dbName");
-  }
-  const job = await this.queue.add('File.exists', `${tyrInstance.options.path}/${dbName}/meta.json`);
-  await job.execution();
 
-  const exists = job.results
-  if(!exists){
-    //This is done to match MemoryAdapter way of failing a find
+module.exports = async function findDatabase(tyrInstance, dbName, opts) {
+  let db;
+  if (!dbName) {
+    throw new Error('Expected dbName');
+  }
+  let job = await this.queue.add('File.exists', `${tyrInstance.options.path}/${dbName}/meta.json`).execution();
+
+  const exists = job.results;
+  if (!exists) {
+    // This is done to match MemoryAdapter way of failing a find
     return db;
   }
-  const meta = await this.queue
-      .add('File.read',`${tyrInstance.options.path}/${dbName}/meta.json`)
-      .getResults();
 
-  db = new Database(Object.assign(meta,{ adapter:this, tyrInstance}));
+  job = await this.queue
+    .add('File.read', `${tyrInstance.options.path}/${dbName}/meta.json`)
+    .execution();
+
+  const meta = job.result;
+
+  db = new Database(Object.assign(meta, { adapter: this, tyrInstance }));
   return db;
-}
+};
